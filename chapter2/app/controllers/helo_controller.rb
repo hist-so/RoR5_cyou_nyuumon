dbeb90f3fc579adbcb763557f5c27721a3f10d50
EC2 Default User <ec2-user@ip-172-31-45-253.ap-southeast-1.compute.internal>
2020-08-06 05:34:04 +0000

    2-2:query-parameter, params[msg]

--- chapter2/app/controllers/helo_controller.rb
+++ chapter2/app/controllers/helo_controller.rb
@@ -1,14 +1,34 @@
 class HeloController < ApplicationController
     def index
+        #p68,
         # render plain: 'Hello this is Rails sample page!'
-        msg =   '
+        
+        #p75,
+        # msg =   '
+        #         <html>
+        #         <body>
+        #             <h1>sample page</h1>
+        #             <p>this is sample page!</p>
+        #         </body>
+        #         </html>
+        #         '
+        # render html: msg.html_safe
+        
+        #p79,
+        if params['msg'] != nil then
+            msg = 'Hello,' + params['msg'] + '!'
+        else
+            msg = 'this is sample page.'
+        end
+        
+        html = '
                 <html>
                 <body>
                     <h1>sample page</h1>
-                    <p>this is sample page!</p>
+                    <p>' + msg + '</p>
                 </body>
                 </html>
-                '
-        render html: msg.html_safe
+              '
+        render html: html.html_safe
     end
 end
