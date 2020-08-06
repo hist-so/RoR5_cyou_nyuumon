cf6d294c167e4d2d13d039c463f60c0527ab7cf2
EC2 Default User <ec2-user@ip-172-31-45-253.ap-southeast-1.compute.internal>
2020-08-06 05:25:24 +0000

    2-2:render html:, .html_safe

--- chapter2/app/controllers/helo_controller.rb
+++ chapter2/app/controllers/helo_controller.rb
@@ -1,5 +1,14 @@
 class HeloController < ApplicationController
     def index
-        render plain: 'Hello this is Rails sample page!'
+        # render plain: 'Hello this is Rails sample page!'
+        msg =   '
+                <html>
+                <body>
+                    <h1>sample page</h1>
+                    <p>this is sample page!</p>
+                </body>
+                </html>
+                '
+        render html: msg.html_safe
     end
 end
