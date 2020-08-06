8948fa9d6b23d2d94127ffc7731cabc804d7e170
EC2 Default User <ec2-user@ip-172-31-45-253.ap-southeast-1.compute.internal>
2020-08-06 05:43:59 +0000

    2-3:view index created.

--- chapter2/app/controllers/helo_controller.rb
+++ chapter2/app/controllers/helo_controller.rb
@@ -15,20 +15,20 @@ class HeloController < ApplicationController
         # render html: msg.html_safe
         
         #p79,
-        if params['msg'] != nil then
-            msg = 'Hello,' + params['msg'] + '!'
-        else
-            msg = 'this is sample page.'
-        end
+        # if params['msg'] != nil then
+        #     msg = 'Hello,' + params['msg'] + '!'
+        # else
+        #     msg = 'this is sample page.'
+        # end
         
-        html = '
-                <html>
-                <body>
-                    <h1>sample page</h1>
-                    <p>' + msg + '</p>
-                </body>
-                </html>
-              '
-        render html: html.html_safe
+        # html = '
+        #         <html>
+        #         <body>
+        #             <h1>sample page</h1>
+        #             <p>' + msg + '</p>
+        #         </body>
+        #         </html>
+        #       '
+        # render html: html.html_safe
     end
 end
