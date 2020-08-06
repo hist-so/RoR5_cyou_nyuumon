7b698fb3ee241762dce7208c63f0706e18b59576
EC2 Default User <ec2-user@ip-172-31-45-253.ap-southeast-1.compute.internal>
2020-08-06 07:23:57 +0000

    2-3:form, post-method

--- chapter2/app/controllers/helo_controller.rb
+++ chapter2/app/controllers/helo_controller.rb
@@ -1,5 +1,5 @@
 class HeloController < ApplicationController
-    def index
+    # def index
         #p68,
         # render plain: 'Hello this is Rails sample page!'
         
@@ -36,15 +36,30 @@ class HeloController < ApplicationController
         # @msg   = 'コントローラーに用意した値です。'
         
         # p88,
-        if params['msg'] != nil then
-            @title = params['msg']
-        else
-            @title = 'index'
-        end
-        @msg = 'this is redirect sample...'
-    end
+    #     if params['msg'] != nil then
+    #         @title = params['msg']
+    #     else
+    #         @title = 'index'
+    #     end
+    #     @msg = 'this is redirect sample...'
+    # end
     
-    def other
-        redirect_to action: :index, params: {'msg': 'from other page'}
+    # def other
+    #     redirect_to action: :index, params: {'msg': 'from other page'}
+    # end
+    
+    # p93
+    
+    protect_from_forgery
+    def index
+        if request.post? then
+            @title = 'Result'
+            @msg   = 'you typed:' + params['input1'] + '.'
+            @value = params['input1']
+        else
+            @title = 'Index'
+            @msg   = 'type text...'
+            @value = ''
+        end
     end
 end
